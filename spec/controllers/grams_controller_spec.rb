d65d116d6d25b83ebd24ec4c10b32a8b4bdf1ba6
gl874391@gmail.com <gl874391@gmail.com>
2020-01-18 17:11:37 +0000

    bootstrap

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe GramsController, type: :controller do
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
+      end
     end
-  end
+end
 
    describe "grams#new action" do
    	 it "should successfully show the new form" do
