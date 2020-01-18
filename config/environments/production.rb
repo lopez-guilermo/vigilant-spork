d22f0454e564107ed043c8affcfc2945c2deb25a
gl874391@gmail.com <gl874391@gmail.com>
2020-01-18 18:59:04 +0000

    devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,14 @@
 Rails.application.configure do
+   config.action_mailer.default_url_options = { host: 'grammable-guillermo-lopez.herokuapp.com' }
+
+  # a bunch of other stuff .....
+end 
+
+
+
+
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
