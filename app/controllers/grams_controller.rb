5a2f92ec9e787ea476be09950508563282acf599
gl874391@gmail.com <gl874391@gmail.com>
2020-01-20 18:39:29 +0000

    editing

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,19 @@
 class GramsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
+
+	def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+     @gram.update_attributes(gram_params)
+
+  if @gram.valid?
+    redirect_to root_path
+  else
+    return render :edit, status: :unprocessable_entity
+  end
+  end
+
+
 	def new
 		@gram = Gram.new
     end
@@ -9,8 +23,14 @@ class GramsController < ApplicationController
 
 	def show
 		@gram = Gram.find_by_id(params{:id})
-		if @gram.blank?
-			    render plain: 'Not Found :(', status: :not_found
+		 return render_not_found if @gram.blank?
+		end
+
+		def edit
+		@gram = Gram.find_by_id(params[:id])
+		return render_not_found if @gram.blank
+		end	
+  
 		end	    
   
 	end	
@@ -28,7 +48,11 @@ class GramsController < ApplicationController
 
   def gram_params
     params.require(:gram).permit(:message)
-   end 
+   end
+
+     def render_not_found
+    render plain: 'Not Found :(', status: :not_found
+  end
 end
  	
 
